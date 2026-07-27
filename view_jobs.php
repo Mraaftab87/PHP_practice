b4bbcfad47081aa6acab40dd3212434fef9fcf9f
Aaftab I gori <goriaaftab87@gmail.com>
2026-07-27 16:53:36 +0530

    fixed some indetions

--- view_jobs.php
+++ view_jobs.php
@@ -5,7 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Jobs</title>
     <style>
-        body { font-family: sans-serif; background-color: #f4f4f4; padding: 20px; }
+        body { 
+            font-family: sans-serif; 
+            background-color: #f4f4f4; 
+            padding: 20px; 
+        }
         .job-grid {
             display: grid;
             grid-template-columns: repeat(4, 1fr);
