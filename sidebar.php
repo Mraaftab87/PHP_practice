df1222779975f1005cc9796eeecc34806d0d4990
Aaftab I gori <goriaaftab87@gmail.com>
2026-09-09 17:54:46 +0530

    Imroving UI exiperience

--- sidebar.php
+++ sidebar.php
@@ -6,6 +6,7 @@
     <title>Admin Dashboard</title>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.ckeditor.com/ckeditor5/39.0.1/classic/ckeditor.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <style>
         .form-card {
             width: 100%;
@@ -112,3 +113,24 @@
         <a href="logout.php" style="color: #ff4c4c;">Logout</a>
     </div>
     <div class="main-content">
+
+        <script>
+            $(document).on('click', '.delete-btn', function(e) {
+                e.preventDefault();
+                const deleteUrl = $(this).attr('href');
+
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#d33',
+                    cancelButtonColor: '#3085d6',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        window.location.href = deleteUrl;
+                    }
+                });
+            });
+        </script>
